4b70b0643675d17f55a6aada8bc3ce332b5177cf
Martin Disch <martindisch@gmail.com>
2024-03-09 20:43:04 +0100

    Parse single-level CSS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use cssparser::{Parser, ParserInput, ToCss};
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is a tests-only project, run `cargo test`.");
+
+    let css = r#"
+.foo {
+  .fancy {
+    color: blue;
+  }
+
+  &:hover {
+    color: orange;
+  }
+}
+
+.bar {
+  color: green;
+}
+"#;
+
+    let mut input = ParserInput::new(css);
+    let mut parser = Parser::new(&mut input);
+    let mut tokens = Vec::new();
+
+    while let Ok(token) = parser.next() {
+        tokens.push(token.clone());
+    }
+
+    println!("{tokens:#?}");
+
+    let css = tokens.iter().map(|t| t.to_css_string()).collect::<String>();
+    println!("{css}");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn complete() {
+        assert!(true);
+    }
 }
